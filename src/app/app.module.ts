117bf2e738672932c379ac334e9020235676ab1f
JMGD1007 <jenniferguerrero1098@gmail.com>
2024-03-16 22:07:22 -0500

    clase2.1 05-marzo-2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaAutosComponent } from './Lista-Autos/Lista-Autos.component';
+import { PaginasComponent } from './paginas/paginas.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ComponentesComponent } from './componentes/componentes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [				
+    AppComponent,
+      ListaAutosComponent,
+      PaginasComponent,
+      ServiciosComponent,
+      ComponentesComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
---
+++ src/app/componentes/componentes.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { ComponentesComponent } from './componentes.component';
+
+describe('ComponentesComponent', () => {
+  let component: ComponentesComponent;
+  let fixture: ComponentFixture<ComponentesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ComponentesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ComponentesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
