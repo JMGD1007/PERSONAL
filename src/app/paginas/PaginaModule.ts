0f963f797efc8720c28704a993d7967ab613f4d0
JMGD1007 <jenniferguerrero1098@gmail.com>
2024-03-17 22:43:34 -0500

    clase2 ok

--- src/app/paginas/PaginaModule.ts
+++ src/app/paginas/PaginaModule.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 
 import { PagListVehiculosComponent } from "./PagListVehiculos/PagListVehiculos.component";
 import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
     imports:[
-        CommonModule
+        CommonModule,
+        FormsModule,
     ],
     declarations:[
         PagListVehiculosComponent,
