50d1c35634a19930c5fd43bc9d52e7c5aea1fee2
JMGD1007 <jenniferguerrero1098@gmail.com>
2024-03-17 23:35:48 -0500

    clase 3.1

--- src/app/paginas/PagListVehiculos/PagListVehiculos.component.ts
+++ src/app/paginas/PagListVehiculos/PagListVehiculos.component.ts
@@ -22,9 +22,9 @@ export class PagListVehiculosComponent implements OnInit {
   }
 
   public listavehiculos: Array<any> = [
-    {"codigo": "A001", "marca": "CHEVROLET", "modelo":"ONIX", "color": "AZUL", "kilometraje": "50000", "precio":"17000", "foto":"https://th.bing.com/th/id/OIP.EmPaXsyNiOvaTmXbapEgLAHaDy?rs=1&pid=ImgDetMain", "anio":"2024", "calificacion":3},
+    {"codigo": "A001", "marca": "CHEVROLET", "modelo":"ONIX-6", "color": "AZUL", "kilometraje": "50000", "precio":"17000", "foto":"https://th.bing.com/th/id/OIP.EmPaXsyNiOvaTmXbapEgLAHaDy?rs=1&pid=ImgDetMain", "anio":"2024", "calificacion":3},
     {"codigo": "A002", "marca": "KIA", "modelo":"RIO", "color": "AZUL", "kilometraje": "50000", "precio":"17000", "foto":"https://www.grupomax.mx/wp-content/uploads/2021/10/rio-sd-22-azul3.png", "anio":"2024", "calificacion":4},
-    {"codigo": "A003", "marca": "CHERY", "modelo":"ARRIZO 5", "color": "AZUL", "kilometraje": "50000", "precio":"17000", "foto":"https://th.bing.com/th/id/OIP.B6WWJU7kiXMiTAjsYDDJ9wHaFS?rs=1&pid=ImgDetMain", "anio":"2024", "calificacion":5},
+    {"codigo": "A003", "marca": "CHERY", "modelo":"ARRIZO-5", "color": "AZUL", "kilometraje": "50000", "precio":"17000", "foto":"https://th.bing.com/th/id/OIP.B6WWJU7kiXMiTAjsYDDJ9wHaFS?rs=1&pid=ImgDetMain", "anio":"2024", "calificacion":5},
     {"codigo": "A004", "marca": "TOYOTA", "modelo":"AGYA", "color": "AZUL", "kilometraje": "50000", "precio":"17000", "foto":"https://s1.cdn.autoevolution.com/images/gallery/TOYOTA-Agya-4995_13.jpg", "anio":"2024", "calificacion":4},
     {"codigo": "A005", "marca": "HYUNDAI", "modelo":"ACCENT", "color": "AZUL", "kilometraje": "50000", "precio":"17000", "foto":"https://th.bing.com/th/id/R.64086b95ab1443ffe710e3e5da210db4?rik=RiDTJckVAE1hLQ&pid=ImgRaw&r=0", "anio":"2024", "calificacion":5},
 
--- src/app/paginas/PaginaModule.ts
+++ src/app/paginas/PaginaModule.ts
@@ -3,11 +3,13 @@ import { NgModule } from "@angular/core";
 import { PagListVehiculosComponent } from "./PagListVehiculos/PagListVehiculos.component";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { UtilitariosModule } from "../utilitarios/UtilitariosModule";
 
 @NgModule({
     imports:[
         CommonModule,
         FormsModule,
+        UtilitariosModule,
     ],
     declarations:[
         PagListVehiculosComponent,
