281fd525fb10887dd87d7cade5d7d5b1cfc10395
JMGD1007 <jenniferguerrero1098@gmail.com>
2024-03-18 22:14:15 -0500

    clase3 navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './paginas/Home/Home.component';
+import { PagListVehiculosComponent } from './paginas/PagListVehiculos/PagListVehiculos.component';
+import { PagNoFoundComponent } from './paginas/PagNotFound/PagNoFound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent
+  }, 
+  {
+    path: "vehiculos",
+    component: PagListVehiculosComponent
+  },
+  {
+    path: "",
+    component: HomeComponent,
+    pathMatch: "full"
+  },
+  {
+    path: "**",
+    component: PagNoFoundComponent,
+    pathMatch: "full"
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
